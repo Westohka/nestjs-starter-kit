d34b4608ac7e890c8a9eedb048408821485ff0dd
Westohka <egovik@live.ru>
2022-05-24 14:46:22 +0700

    add sample for e2e test

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+
+import * as request from 'supertest';
+
+import { AppModule } from '../src/app/app.module';
+
+jest.mock('../src/app/queues/consumers/separate/separate.module');
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/template')
+      .expect(200)
+      .expect('Hello World!');
+  });
+});
