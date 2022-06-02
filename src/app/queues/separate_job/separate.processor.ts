5c2449418978604507232918fdd4dfc67e74ef04
Westohka <egovik@live.ru>
2022-06-02 10:30:16 +0700

    add application context use case example to separate processor

--- src/app/queues/separate_job/separate.processor.ts
+++ src/app/queues/separate_job/separate.processor.ts
@@ -1,10 +1,39 @@
 import { Job, DoneCallback } from 'bull';
 
-async function start(job: Job, cb: DoneCallback): Promise<void> {
-  console.log(`[${process.pid}] ${JSON.stringify(job.data)}`);
-  cb(null, null);
+import { NestFactory } from '@nestjs/core';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import UserRepository from '../../database/repositories/user.repository';
+
+import DatabaseModule from '../../database/database.module';
+
+@Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([UserRepository])],
+})
+class AppContextModule {}
+
+class SeparateProcessor {
+  async start(job: Job, cb: DoneCallback): Promise<void> {
+    console.log(`[${process.pid}] ${JSON.stringify(job.data)}`);
+
+    const app = await NestFactory.createApplicationContext(AppContextModule, {
+      logger: false,
+    });
+
+    const repository = app.get<UserRepository>(UserRepository);
+
+    const users = await repository.findAndCount({
+      take: 10,
+    });
+
+    console.log('users', users);
+
+    cb(null, null);
+  }
 }
 
 export default function (job: Job, cb: DoneCallback) {
-  start(job, cb);
+  const processor = new SeparateProcessor();
+  processor.start(job, cb);
 }
